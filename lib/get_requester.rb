fb992a297e3d2e1ea088b5eef017edd5c9f48129
zachary mines <zmines@wesleyan.edu>
2020-07-26 20:00:06 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+
+class GetRequester
+  attr_accessor :url 
+  def initialize(url)
+    @url = url 
+  end 
+  
+  def get_response_body 
+    a = Net::HTTP.get_response(URI.parse(url))
+    a.body 
+  end 
+  
+  def parse_json 
+    JSON.parse(get_response_body)
+  end 
+end 
